57d68c5d48ec76dcf48cecec305213ba32743bf5
parjjinhee53 <parkjh6275@naver.com>
2023-01-01 21:15:46 +0900

    프로젝트 파일 생성

---
+++ ToBeList/ToBeList/ToBeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToBeListApp.swift
+//  ToBeList
+//
+//  Created by Jinhee on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct ToBeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
